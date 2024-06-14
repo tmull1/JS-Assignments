fd32c5c6b81cb133fbbad422bd9db5f5db0d93e6
tmull1 <tmull1732@gmail.com>
2024-06-14 09:51:02 -0400

    RPS2 Complete

--- RPS.js
+++ RPS.js
@@ -1,11 +1,8 @@
-
 const hands = ['rock', 'paper', 'scissors'];
-
 
 function getHand() {
     return hands[parseInt(Math.random() * 10) % 3];
 }
-
 
 const player1 = {
     name: 'Player 1',
@@ -17,13 +14,20 @@ const player2 = {
     getHand: getHand
 };
 
+const player3 = {
+    name: 'Player 3',
+    getHand: getHand
+};
+
+const player4 = {
+    name: 'Player 4',
+    getHand: getHand
+};
 
 function playRound(player1, player2) {
-    
     const hand1 = player1.getHand();
     const hand2 = player2.getHand();
 
-    
     let winner = null;
     if (hand1 === hand2) {
         console.log(`${hand1} vs ${hand2} - It's a tie!`);
@@ -37,14 +41,34 @@ function playRound(player1, player2) {
         winner = player2;
     }
 
-    
     return winner;
 }
 
+function playGame(player1, player2, playUntil) {
+    let score1 = 0;
+    let score2 = 0;
 
-const winner = playRound(player1, player2);
-if (winner) {
-    console.log(`The winner is ${winner.name}`);
-} else {
-    console.log('No winner this round.');
+    while (score1 < playUntil && score2 < playUntil) {
+        const winner = playRound(player1, player2);
+        if (winner === player1) {
+            score1++;
+        } else if (winner === player2) {
+            score2++;
+        }
+        console.log(`${player1.name}: ${score1} - ${player2.name}: ${score2}`);
+    }
+
+    return score1 === playUntil ? player1 : player2;
 }
+
+function playTournament(players, playUntil) {
+    const semiFinal1Winner = playGame(players[0], players[1], playUntil);
+    const semiFinal2Winner = playGame(players[2], players[3], playUntil);
+
+    const finalWinner = playGame(semiFinal1Winner, semiFinal2Winner, playUntil);
+
+    console.log(`${finalWinner.name} is the world champion`);
+}
+
+const players = [player1, player2, player3, player4];
+playTournament(players, 3);
